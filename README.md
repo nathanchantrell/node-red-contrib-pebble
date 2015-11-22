fab34c8b0d073106da049e205a9a2b2b37e5fd63
Nathan Chantrell <nathan@chantrell.net>
2015-11-22 13:08:16 +0000

    Initial version

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
-# node-red-contrib-pebble
+node-red-contrib-pebble
+=======================
+
+A <a href="http://nodered.org">Node-RED</a> node to send pins and optional nofifitcations to the Timeline on a Pebble smartwatch.
+
+Install
+-------
+
+Run the following command in the root directory of your Node-RED install
+
+    npm install node-red-contrib-pebble
+
+
+Usage
+-----
+
+Uses **msg.time** to set the time for the pin (required).
+
+Optionally uses **msg.payload** to set the title, if not already set in the properties.
+
+Optionally uses **msg.duration** to set the duration.
+
+Optionally uses **msg.body** to set the body text of the pin
+
+Optionally set **msg.notify** to true to get a notification when the pin is created
+
+The Pebble User Token is stored in a separate credentials file.
+
---
+++ pebble.js
@@ -0,0 +1,84 @@
+/**
+ * Copyright 2015 Nathan Chantrell @nathanchantrell
+ * http://nathan.chantrell.net
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ **/
+
+module.exports = function(RED) {
+    "use strict";
+    var Timeline = require('pebble-api').Timeline;
+    var timeline = new Timeline();
+
+    function PebbleNode(n) {
+        RED.nodes.createNode(this,n);
+        this.title = n.title;
+        var credentials = this.credentials;
+        if ((credentials) && (credentials.hasOwnProperty("pebbletoken"))) { this.pebbletoken = credentials.pebbletoken; }
+        else { this.error("No Pebble User Token set"); }
+        var node = this;
+        this.on("input",function(msg) {
+            var pinTitle = this.title||msg.payload||"Node-RED";
+            if (node.pebbletoken) {
+		var userToken = node.pebbletoken;
+
+		if (msg.notify == true) {
+        	        var pin = new Timeline.Pin({
+			  id: Date.now().toString(),
+	                  time: new Date(msg.time),
+	                  duration: msg.duration,
+			  createNotification: {
+			    layout: ({
+			     type: 'genericNotification',
+			     tinyIcon: Timeline.Pin.Icon.NOTIFICATION_FLAG,
+			     title: pinTitle,
+			     body: msg.body
+			    })
+			  },
+        	          layout: new Timeline.Pin.Layout({
+	                    type: Timeline.Pin.LayoutType.GENERIC_PIN,
+	                    tinyIcon: Timeline.Pin.Icon.NOTIFICATION_FLAG,
+	                    title: pinTitle,
+	                    body: msg.body
+	                  })
+	                });
+		} else {
+			var pin = new Timeline.Pin({
+			  time: new Date(msg.time),
+			  duration: msg.duration,
+			  layout: new Timeline.Pin.Layout({
+			    type: Timeline.Pin.LayoutType.GENERIC_PIN,
+			    tinyIcon: Timeline.Pin.Icon.NOTIFICATION_FLAG,
+			    title: pinTitle,
+			    body: msg.body
+			  })
+			});
+	}
+		timeline.sendUserPin(userToken, pin, function (err) {
+		  if (err) {
+		    node.warn("Pebble Timeline error: " + err);
+		  }
+		});
+            }
+            else {
+                node.warn("Pebble User Token credentials not set.");
+            }
+        });
+    }
+
+    RED.nodes.registerType("pebble",PebbleNode, {
+        credentials: {
+            pebbletoken: {type: "password"}
+        }
+    });
+}
